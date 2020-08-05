d286ed408b042c040241d5ed68b447a2c5e6e063
Daniel-Penaloza <danielpenalozatj@gmail.com>
2020-08-04 17:52:56 -0700

    Handling success

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -12,20 +12,25 @@ module ExpenseTracker
         end
 
         let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+        let(:expense) = { { 'some' => 'data' } }
+        before do
+            allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
 
         describe 'POST /expenses' do
             context 'when the expense is successfully recorded' do
                 it 'returns the expense id' do
-                    expense = { 'some' => 'data' }
-
-                    allow(ledger).to receive(:record)
-                    .with(expense)
-                    .and_return(RecordResult.new(true, 417, nil))
-
                     post '/expenses', JSON.generate(expense)
-
+                    
                     parsed = JSON.parse(last_response.body)
                     expect(parsed).to include('expense_id' => 417)
+                end
+
+                it 'responds with a 200(OK)' do
+                    post '/expenses', JSON.generate(expense)
+                    expect(last_response.status).to eq(200)
                 end
             end
 
